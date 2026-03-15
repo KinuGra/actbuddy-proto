60ded51958bd4f8788f9930ced1add23a56bc5eb
mimimimi2002 <mimimimi2002.ma@gmail.com>
2026-03-15 22:37:55 +0900

    feat: handlerの作成

---
+++ backend/internal/chat/websocket/handler.go
@@ -0,0 +1,40 @@
+package websocket
+
+// handle upgrader
+// register client to hub
+// send data to hub
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// upgrader: HTTP to websocket
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// upgrade HTTP to websocket and register client to hub
+func ServeWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println("Websocket upgrade error")
+		return
+	}
+
+	// create Client
+	client := &Client{
+		hub: hub,
+		conn: conn,
+		send: make(chan []byte, 256),
+	}
+	hub.register <- client
+
+	go client.writePump()
+	client.readPump()
+}
