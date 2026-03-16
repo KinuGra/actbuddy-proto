39507a34baef58f6d142800d90f980c291d43214
mimimimi2002 <mimimimi2002.ma@gmail.com>
2026-03-16 23:59:23 +0900

    feat: ClientでMessageを受け取りサーバーに保存した後hubのbroadcastに追加(サーバーなし)

--- backend/internal/chat/websocket/client.go
+++ backend/internal/chat/websocket/client.go
@@ -1,18 +1,36 @@
 package websocket
 
 import (
+	"encoding/json"
 	"github.com/gorilla/websocket"
 	"log"
+	"time"
 )
 
 type Client struct {
+	id   int64
 	hub  *Hub
 	conn *websocket.Conn
 	send chan []byte
 }
 
-func NewClient(hub *Hub, conn *websocket.Conn, send chan []byte) *Client {
-	return &Client{hub: hub, conn: conn, send: send}
+type ReadMessage struct {
+	RoomID   int64  `json:"room_id"`
+	SenderID int64  `json:"sender_id"`
+	Content  string `json:"content"`
+}
+
+type SavedMessage struct {
+	MessageID  int64     `json:"message_id"`
+	RoomID     int64     `json:"room_id"`
+	SenderID   int64     `json:"sender_id"`
+	SenderName string    `json:"sender_name"`
+	Content    string    `json:"content"`
+	CreatedAt  time.Time `json:"created_at"`
+}
+
+func NewClient(id int64, hub *Hub, conn *websocket.Conn, send chan []byte) *Client {
+	return &Client{id: id, hub: hub, conn: conn, send: send}
 }
 
 func (c *Client) readPump() {
@@ -24,23 +42,40 @@ func (c *Client) readPump() {
 	}()
 
 	for {
-		_, message, err := c.conn.ReadMessage()
+		_, msgBytes, err := c.conn.ReadMessage()
 		if err != nil {
 			log.Println("Read error:", err)
 			break
 		}
-		log.Printf("Message received: %s", message)
-		c.hub.broadcast <- message
+		var readMessage ReadMessage
+		err = json.Unmarshal(msgBytes, &readMessage)
+		if err != nil {
+			log.Println("JSON unmarshal error:", err)
+			continue
+		}
+
+		// サーバーに保存
+		savedMessage := SavedMessage{
+			MessageID:  100,
+			RoomID:     readMessage.RoomID,
+			SenderID:   readMessage.SenderID,
+			SenderName: "テストユーザー",
+			Content:    readMessage.Content,
+			CreatedAt:  time.Date(2026, 3, 14, 12, 0, 0, 0, time.UTC),
+		}
+		// サーバーから返ってきたデータをbroadcast
+		log.Printf("Saved Message at DB as message_id :%d, room_id %d, sender_id %d, sender_name %s, content : %s, created_at : %v", savedMessage.MessageID, savedMessage.RoomID, savedMessage.SenderID, savedMessage.SenderName, savedMessage.Content, savedMessage.CreatedAt)
+		c.hub.broadcast <- &savedMessage
 	}
 }
 
 func (c *Client) writePump() {
-	for message := range c.send {
-		err := c.conn.WriteMessage(websocket.TextMessage, message)
+	for savedMessage := range c.send {
+		err := c.conn.WriteMessage(websocket.TextMessage, savedMessage)
 		if err != nil {
 			log.Println("Write error:", err)
 		}
 
-		log.Printf("Message sent: %s", message)
+		log.Printf("Message sent: %s", savedMessage)
 	}
 }
